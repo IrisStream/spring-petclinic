7a99426ed6e687c67131a3ff684d997b0dcdd458
Iris Stream <ngodaisonn@gmail.com>
2022-05-09 16:05:32 +0700

    Fix health check pipeline

--- jenkins-pipeline/scripts/health_check.sh
+++ jenkins-pipeline/scripts/health_check.sh
@@ -4,7 +4,7 @@ RESPONCE="$(curl -s --head --request GET deploy:8080 | grep 200)"
 
 echo "Responce: $RESPONCE"
 
-if (($RESPONCE != "")); then
+if [$RESPONCE != ""]; then
     echo "Don't rollback"
 else
     ./jenkins-pipeline/scripts/rollback.sh
