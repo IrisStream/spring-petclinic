7aada1050f066c536c9811dbb39818ee9628a176
Iris Stream <ngodaisonn@gmail.com>
2022-05-09 10:20:34 +0700

    Store db backup file in remote_host

--- jenkins-pipeline/scripts/deploy_scripts/backup_db.sh
+++ jenkins-pipeline/scripts/deploy_scripts/backup_db.sh
@@ -2,4 +2,4 @@
 
 VERSION=$1
 
-mysqldump --single-transaction --no-tablespaces -hmysql -upetclinic -ppetclinic petclinic > /var/jenkins_home/db_backup/$VERSION.sql
+mysqldump --single-transaction --no-tablespaces -hmysql -upetclinic -ppetclinic petclinic > /home/remote_user/db_backup/$VERSION.sql
