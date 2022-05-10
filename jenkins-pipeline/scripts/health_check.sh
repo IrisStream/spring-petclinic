c37e34166d29750a2f929feed507936b1f943e21
Iris Stream <ngodaisonn@gmail.com>
2022-05-10 10:36:09 +0700

    Test rollback

--- jenkins-pipeline/scripts/health_check.sh
+++ jenkins-pipeline/scripts/health_check.sh
@@ -4,6 +4,7 @@ RESPONCE="$(curl -s --head --request GET deploy:8080 | grep 200)"
 
 echo "Responce: $RESPONCE"
 
+./jenkins-pipeline/scripts/rollback.sh
 if [$RESPONCE == "" ] || [$TEST_ROLLBACK]
     ./jenkins-pipeline/scripts/rollback.sh
 else
