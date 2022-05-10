29b02169bc784cbeb16b94f03a6fd7dbaf005d0f
Iris Stream <ngodaisonn@gmail.com>
2022-05-10 11:26:17 +0700

    Test restore db

--- jenkins-pipeline/scripts/deploy_scripts/run_jar.sh
+++ jenkins-pipeline/scripts/deploy_scripts/run_jar.sh
@@ -17,7 +17,7 @@ DEPLOY_IMAGE_NAME=$(python3 get_name_of_snapshot.py)
 
 docker build --build-arg JAR_NAME=${DEPLOY_IMAGE_NAME} -t $DEPLOY_IMAGE_NAME --network=jenkins_lab_jenkins_net .
 
-./backup_db.sh $DEPLOY_IMAGE_NAME
+#./backup_db.sh $DEPLOY_IMAGE_NAME
 
 docker run -e SPRING_PROFILES_ACTIVE=mysql -d -p 8083:8080 --name deploy --network jenkins_lab_jenkins_net $DEPLOY_IMAGE_NAME
 
