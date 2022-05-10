8b7bf15c4f79df00bb9c210dce281f4f5179a95e
Iris Stream <ngodaisonn@gmail.com>
2022-05-10 12:01:17 +0700

    Stop container before rollback

--- jenkins-pipeline/scripts/deploy_scripts/restore_to_prev_version.sh
+++ jenkins-pipeline/scripts/deploy_scripts/restore_to_prev_version.sh
@@ -15,6 +15,13 @@ mysql -hmysql -upetclinic -ppetclinic petclinic < ../db_backup/$OLD_VERSION.sql
 
 export DOCKER_HOST="tcp://172.20.0.1:2375"
 
+if [ "$(docker ps -aq -f name=deploy)" ]; then 
+	if (("$(docker container inspect -f '{{.State.Running}}' deploy)" == true)); then
+        docker stop deploy
+	fi
+	docker rm deploy
+fi
+
 docker run -e SPRING_PROFILES_ACTIVE=mysql -d -p 8083:8080 --name deploy --network jenkins_lab_jenkins_net $OLD_VERSION
 
 popd
