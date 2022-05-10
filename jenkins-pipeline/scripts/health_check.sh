b001f60e5d59174f12eab1dcc4ff64e4c5fbf911
Iris Stream <ngodaisonn@gmail.com>
2022-05-10 10:50:23 +0700

    Fix if syntax

--- jenkins-pipeline/scripts/health_check.sh
+++ jenkins-pipeline/scripts/health_check.sh
@@ -4,8 +4,8 @@ RESPONCE="$(curl -s --head --request GET deploy:8080 | grep 200)"
 
 echo "Responce: $RESPONCE"
 
-./jenkins-pipeline/scripts/rollback.sh
-if [$RESPONCE == "" ] || [$TEST_ROLLBACK]
+if [ -z "$RESPONCE" ] || [ $TEST_ROLLBACK ];
+then
     ./jenkins-pipeline/scripts/rollback.sh
 else
     echo "Health check fine!"
